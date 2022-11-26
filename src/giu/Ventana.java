f814b6f414236f69d4c78275baa62156fa653965
Gianfranco Impedovo <impefranco98@gmail.com>
2022-11-26 11:28:02 -0300

    modularizo la ventana

--- src/giu/Ventana.java
+++ src/giu/Ventana.java
@@ -29,7 +29,6 @@ public class Ventana extends JFrame{
 	private JLabel titulo, txtMaquina, txtOpciones, txtCredito, datosMaquina;
 	private JTextField credito;
 	private JButton aceptar;
-	private Timer timer;
 	
 	
 	public Ventana() throws MaquinaExcepcion {
@@ -42,7 +41,11 @@ public class Ventana extends JFrame{
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		// Container con grilla
+		configuracion();
+		
+	} 
+	
+	public void configuracion() throws MaquinaExcepcion {
 		Container c = this.getContentPane();
 		c.setLayout(new GridLayout(1,3));
 		
@@ -50,12 +53,28 @@ public class Ventana extends JFrame{
 		titulo = new JLabel("Casino", SwingConstants.CENTER);
 		titulo.setFont(new Font("Serif", Font.PLAIN, 40));
 		
-		ImageIcon maquinaI = new ImageIcon(getClass().getResource("/img/maquina_200x200.png"));
+        Integer[] nros = {Casino.getInstancia().getMaquinaView(1).getNroMaquina(),
+				 Casino.getInstancia().getMaquinaView(2).getNroMaquina(),
+			     Casino.getInstancia().getMaquinaView(3).getNroMaquina()};
+        JComboBox<Integer> nroMaquinas = new JComboBox<>(nros);
+        ((JLabel)nroMaquinas.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
 		
-		JPanel panel_Lbl = new JPanel(new GridLayout(5,1));
-		JPanel panel_Opciones = new JPanel(new GridLayout(5,1, 70, 70));
-		JPanel panel_Datos = new JPanel(new GridLayout(3,1));
 		
+		JPanel panel_Lbl = inicializaPanelDeLbl1();
+		JPanel panel_Opciones = inicializaPanelDeOpciones(nroMaquinas);
+		JPanel panel_Datos = inicializaPanelDeDatos(nroMaquinas);
+
+        
+		
+		c.add(panel_Lbl);
+		c.add(panel_Opciones);
+		c.add(panel_Datos);
+		
+	}
+	
+	public JPanel inicializaPanelDeLbl1() {
+		
+		JPanel panelLbl = new JPanel(new GridLayout(5,1));
 		txtMaquina = new JLabel("Maquina", SwingConstants.CENTER);
 		txtMaquina.setFont(new Font("Serif", Font.BOLD, 30));
 		txtOpciones = new JLabel("Opciones", SwingConstants.CENTER);
@@ -63,21 +82,20 @@ public class Ventana extends JFrame{
 		txtCredito = new JLabel("Crédito", SwingConstants.CENTER);
 		txtCredito.setFont(new Font("Serif", Font.BOLD, 30));
 		
-		panel_Lbl.add(new JLabel());
-		panel_Lbl.add(txtMaquina);
-		panel_Lbl.add(txtOpciones);
-		panel_Lbl.add(txtCredito);
-		panel_Lbl.add(new JLabel());
+		panelLbl.add(new JLabel());
+		panelLbl.add(txtMaquina);
+		panelLbl.add(txtOpciones);
+		panelLbl.add(txtCredito);
+		panelLbl.add(new JLabel());
+		
+		return panelLbl;
+	}
+	
+	public JPanel inicializaPanelDeOpciones(JComboBox<Integer> nroMaquinas) throws MaquinaExcepcion {
+		JPanel panel_Opciones = new JPanel(new GridLayout(5,1, 70, 70));
 		
 		titulo = new JLabel("Casino", SwingConstants.CENTER);
 		titulo.setFont(new Font("Serif", Font.BOLD, 40));
-
-		
-        Integer[] nros = {Casino.getInstancia().getMaquinaView(1).getNroMaquina(),
-        				 Casino.getInstancia().getMaquinaView(2).getNroMaquina(),
-        			     Casino.getInstancia().getMaquinaView(3).getNroMaquina()};
-        JComboBox<Integer> nroMaquinas = new JComboBox<>(nros);
-        ((JLabel)nroMaquinas.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
         
         String[] listaOpciones = {"Jugar", "Dar Alta Premio", "Dar Baja Premio"};
         JComboBox<String>  opciones = new JComboBox<>(listaOpciones);
@@ -91,10 +109,15 @@ public class Ventana extends JFrame{
         panel_Opciones.add(opciones);
         panel_Opciones.add(credito);
         panel_Opciones.add(aceptar);
-		
         
+        return panel_Opciones;
+	}
+	
+	public JPanel inicializaPanelDeDatos(JComboBox<Integer> nroMaquinas) throws MaquinaExcepcion {
+		JPanel panel_Datos = new JPanel(new GridLayout(3,1));
         datosMaquina = new JLabel(Casino.getInstancia().getMaquinaView(1).toString(), SwingConstants.CENTER);
         datosMaquina.setFont(new Font("Serif", Font.BOLD, 20));
+        
         nroMaquinas.addActionListener(new ActionListener() {
 
 			@Override
@@ -111,16 +134,14 @@ public class Ventana extends JFrame{
         	
         });
         
+        ImageIcon maquinaI = new ImageIcon(getClass().getResource("/img/maquina_200x200.png"));
+        
         panel_Datos.add(datosMaquina);
         panel_Datos.add(new JLabel(maquinaI));        
         panel_Datos.add(new JLabel());
         
-		
-		c.add(panel_Lbl);
-		c.add(panel_Opciones);
-		c.add(panel_Datos);
-		
+        return panel_Datos;
 	}
-
-
 }
+
+
